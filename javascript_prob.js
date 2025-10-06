bce32573a9e1ca11c99183e49fa02ed00d7d0466
Itslrrn <larrenellajoydizon@gmail.com>
2025-10-06 10:21:51 +0800

    pull latest update.txt

--- javascript_prob.js
+++
@@ -1,7 +0,0 @@
-// Update skills
-currentUser.skills = [];
-document.querySelectorAll('.skill-input').forEach(input => {
-    if (input.value.trim() !== '') {
-        currentUser.skills.push(input.value.trim());
-    }
-});
